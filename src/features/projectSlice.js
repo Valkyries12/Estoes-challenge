34af4a73c3d120f175f4ac521104512cd53fcc7f
Nicolás Maximiliano Caruso <nico.caruso1992@outlook.com>
2022-04-08 15:18:51 -0300

    creacion projectSlice

---
+++ src/features/projectSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, current } from '@reduxjs/toolkit';
+
+
+export const projectsSlice = createSlice({
+  name: 'projects',
+  initialState: {
+    projects: [
+      {id: 1, projectInfo: "Landing page", projectManager: "Walt Cossani", assignedTo: "Ignacio Truffa", status: "enabled"},
+      {id: 2, projectInfo: "E-commerce shop", projectManager: "Walt Cossani", assignedTo: "Ignacio Truffa", status: "enabled"},
+      {id: 3, projectInfo: "CRM linkroom", projectManager: "Walt Cossani", assignedTo: "Ignacio Truffa", status: "enabled"}
+    ],
+  },
+  reducers: {
+    deleteProject: (state, action) => {
+      const currentState = current(state);
+      const filteredProject = currentState.projects.filter(item => item.id !== action.payload);
+
+      return {
+        ...currentState,
+        projects: filteredProject
+      };
+    },
+    addProject: (state, action) => {
+      const currentState = current(state);
+      return {
+        ...currentState,
+        projects: action.payload
+      }
+    }
+  }
+});
+
+export const { deleteProject, addProject } = projectsSlice.actions;
+
+export default projectsSlice.reducer;
